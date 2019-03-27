b7980f7418d3622b3a46740e8b7089b6b8366c5c
JosephSIN <joseph.sin@yandex.by>
2019-03-27 08:04:23 +0300

    hw5

---
+++ hw05/EpamTrnHw05/EpamTrnHw05/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EpamTrnHw05
+{
+    class Program
+    {
+        const int COUNT_REPLACEMENTS = 25000;
+        const int MIN_WORD_LENGTH = 5;
+
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"goncharov_ivan-obyknovennaja_istorija.txt", Encoding.Default);
+            string[] words = text.Split();
+
+            Dictionary<string, string> wordReplacements = new Dictionary<string, string>();
+            Dictionary<string, string> wordReplacementsReverse = new Dictionary<string, string>();
+            char replaceSymbol = Convert.ToChar(2000);
+
+            foreach (string word in words)
+            {
+                if (wordReplacements.Count > COUNT_REPLACEMENTS)
+                {
+                    break;
+                }
+
+                if (word.Length >= MIN_WORD_LENGTH && !wordReplacements.ContainsKey(word))
+                {
+                    wordReplacements.Add(word, replaceSymbol.ToString());
+                    wordReplacementsReverse.Add(replaceSymbol.ToString(), word);
+                    replaceSymbol++;
+                }
+            }
+
+            foreach (string word in wordReplacements.Keys)
+            {
+                text = text.Replace(word, wordReplacements[word]);
+            }
+
+            File.WriteAllText(@"pack.txt", text);
+            string packedText = File.ReadAllText(@"pack.txt");
+
+            foreach (string symbol in wordReplacementsReverse.Keys)
+            {
+                packedText = packedText.Replace(symbol, wordReplacementsReverse[symbol]);
+            }
+
+            File.WriteAllText(@"unpack.txt", packedText);
+        }
+    }
+}
